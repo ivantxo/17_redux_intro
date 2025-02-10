e507409edb1944c8b2b47cf967b91673fc96aa3c
IvanR <ivan.ramirezf@gmail.com>
2025-02-10 23:02:18 +1000

    Added action creator functions

--- src/store.js
+++ src/store.js
@@ -35,7 +35,7 @@ function reducer(state = initialState, action) {
       return {
         ...state,
         loan: 0,
-        loadPurpose: "",
+        loanPurpose: "",
         balance: state.balance - state.loan,
       };
 
@@ -46,16 +46,31 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 600 });
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
 
-store.dispatch({ type: "account/withdraw", payload: 200 });
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(600));
+store.dispatch(withdraw(200));
 console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+store.dispatch(requestLoan(1000, "Buy a cheap car"));
 console.log(store.getState());
 
-store.dispatch({ type: "account/payloan" });
+store.dispatch(payLoan());
 console.log(store.getState());
