9f288821f9c5bef54823f74fadc42245c3ce20cd
IvanR <ivan.ramirezf@gmail.com>
2025-02-15 00:20:23 +1000

    Added isLoading
    - Added isLoading to deposit button

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -2,7 +2,7 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
-  balance: 0,
+  isLoading: false,
 };
 
 export default function accountReducer(state = initialStateAccount, action) {
@@ -11,6 +11,7 @@ export default function accountReducer(state = initialStateAccount, action) {
       return {
         ...state,
         balance: state.balance + action.payload,
+        isLoading: false,
       };
 
     case "account/withdraw":
@@ -37,6 +38,9 @@ export default function accountReducer(state = initialStateAccount, action) {
         balance: state.balance - state.loan,
       };
 
+    case "account/convertingCurrency":
+      return { ...state, isLoading: true };
+
     default:
       return state;
   }
@@ -46,6 +50,7 @@ export function deposit(amount, currency) {
   if (currency === "USD") return { type: "account/deposit", payload: amount };
 
   return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
     // API call
     const res = await fetch(
       `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
